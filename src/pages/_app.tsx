ac8b3b2c8ec8e567f792893db37984fb594775d6
Alifiyah Shahid <shahid.alifiyah@gmail.com>
2024-12-01 14:14:51 -0500

    Contact updated

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,27 @@
+// import type { AppProps } from 'next/app';
+// import '../styles/globals.css';
+
+// function MyApp({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />;
+// }
+
+// export default MyApp;
+
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Alifiyah Shahid Portfolio</title>
+        <meta name="description" content="Portfolio of Alifiyah Shahid, showcasing projects, skills, and achievements in software development." />
+        <link rel="icon" href="/portfolio.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
-
